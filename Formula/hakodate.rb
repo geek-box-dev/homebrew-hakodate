c16c554e63e4bef0d2d8a8752237bf72ed699294
akihisa-sumi <sumi.akihisa@gmail.com>
2021-10-18 21:01:07 +0900

    Update hakodate.rb

--- Formula/hakodate.rb
+++ Formula/hakodate.rb
@@ -1,7 +1,7 @@
 class Hakodate < Formula
   homepage "https://github.com/geek-box-dev/hakodate"
   url "https://github.com/geek-box-dev/hakodate/releases/download/0.0.2/hakodate"
-  sha256 "6b59a1fe7bdd55ff5c8125ba717550d7b1ab9d937d3efd0a001bbfdc3e4da39b"
+  sha256 "781b7bef39a1c155814c1182066d6f106bf990b9782b3070bcefe2cc5c26837f"
   license "MIT"
 
   def install
